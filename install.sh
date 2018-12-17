8e747d8734f083e73b7e3df57e28182df1ab5e35
Jeff Cox <jcox@paloaltonetworks.com>
2018-12-17 11:51:55 -0800

    Update sudo usage, need to break up detection and installation now

--- install.sh
+++ install.sh
@@ -1,14 +1,28 @@
 #! /usr/bin/env bash
 # Determine host OS and if possible perform the steps for a Minemeld install
 
-# TODO: Figure out something more elegant for privelege escalation
-# Littering this with sudos is dumb
+# Check for and require root execution
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+
+if [ $SUDO_USER ]; then
+    real_user=$SUDO_USER
+else
+    real_user=$(whoami)
+fi
 
 # Init and user prompts
 echo "This script will attempt to install Minemeld for your distro"
 
 echo "Palo Alto Networks recommends using the develop branch."
 echo "Would you like to use develop or master?"
+
+echo "Add your account to the minemeld group?"
+echo "(Recommended for development and troubleshooting)"
+
+# TODO: Split detection and installation account for not having lsb_release
 
 # Apt based?
 # Needs testing
@@ -17,14 +31,14 @@ if [[ -x $(which apt-get) ]]; then
         echo -e "Detectd Ubuntu"
         if [ $(lsb_release -rs) ==  "14.04"]; then
             echo -e "Version 14.04 is supported, proceeding"
-            sudo apt-get update
-            sudo apt-get upgrade
-            sudo apt-get install -y gcc git python2.7-dev libffi-dev libssl-dev make
+            apt-get update
+            apt-get upgrade
+            apt-get install -y gcc git python2.7-dev libffi-dev libssl-dev make
         elif [ $(lsb_release -rs) ==  "16.04"]; then
             echo -ne "Version 16.04 is supported, proceeding"
-            sudo apt-get update
-            sudo apt-get upgrade
-            sudo apt-get install -y gcc git python-minimal python2.7-dev libffi-dev libssl-dev make
+            apt-get update
+            apt-get upgrade
+            apt-get install -y gcc git python-minimal python2.7-dev libffi-dev libssl-dev make
         else; then
             echo "Sorry, did not detect a supported version"
         fi
@@ -32,9 +46,9 @@ if [[ -x $(which apt-get) ]]; then
         echo -e "Detected Debian"
         if [ $(lsb_release -rs) ==  "7" ] or [ $(lsb_release -rs) ==  "9" ]; then
             echo -e "Debian 7 and 9 are supported, proceeding"
-            sudo apt-get update
-            sudo apt-get upgrade # optional
-            sudo apt-get install -y gcc git python2.7-dev libffi-dev libssl-dev
+            apt-get update
+            apt-get upgrade # optional
+            apt-get install -y gcc git python2.7-dev libffi-dev libssl-dev
         fi
     else;
         echo "Sorry dude, you're not supported"
@@ -48,7 +62,7 @@ if [[ -x $(which yum) ]]; then
     if [ $(lsb_release -is) == "CentOS" ] and [ $(lsb_release -rs) == "7" ]; then
         echo -e "Detected CentOS"
         echo -e "CentOS 7 is supported, proceeding"
-        sudo yum install -y wget git gcc python-devel libffi-devel openssl-devel
+        yum install -y wget git gcc python-devel libffi-devel openssl-devel
     fi
 fi
 
@@ -81,7 +95,7 @@ ansible-playbook -K -i 127.0.0.1, minemeld-ansible/local.yml
 
 # Add the user who started this to minemeld group
 # check $PATH and sudo requirements for Debian, was 'sudo /usr/sbin/usermod'
-usermod -a -G minemeld <your user> # add your user to minemeld group, useful for development
+usermod -a -G minemeld ${real_user} # add your user to minemeld group, useful for development
 
 # Clean up
 
